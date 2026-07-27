ca6fbe356af241024d9be17b915e47ff72bd9172
waqar-gl <waqar.shaikh@get-licensed.co.uk>
2026-07-27 16:04:17 +0500

    Delete core directory

--- core/config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-class Config:
-    def __init__(self, path="config.yaml"):
-        with open(path, "r") as f:
-            self.data = yaml.safe_load(f)
-    def __getitem__(self, item):
-        return self.data[item]
--- core/constants.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class Severity(Enum):
-    CRITICAL = 4
-    HIGH = 3
-    MEDIUM = 2
-    LOW = 1
